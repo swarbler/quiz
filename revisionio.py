7cb03f83cccada6c7911392965dc6794d5ff14a3
sbird <sbird@disroot.org>
2024-11-21 10:12:04 +0800

    topic selection now uses match and case instead of if and else if statements

--- revisionio.py
+++ revisionio.py
@@ -64,26 +64,27 @@ def set_subject(subject='example', topic='none'):
 
     if subject == 'geography':
         # different topics in geography
-        if topic == 'population and settlements' or topic == 'population' or topic == 'populations' or topic == 'settlements' or topic == 'settlement': # different words for same topic
-            subtopic = 'population'
-        elif topic == 'volcanoes' or topic == 'volcano': # plural or singular
-            subtopic = 'volcanoes'
-        elif topic == 'earthquakes' or topic == 'earthquake':
-            subtopic = 'earthquakes'
-        elif topic == 'rivers' or topic == 'river':
-            subtopic = 'rivers'
-        elif topic == 'coasts' or topic == 'coast':
-            subtopic = 'coasts'
-        elif topic == 'weather and climate' or topic == 'weather' or topic == 'climate':
-            subtopic = 'weather'
-        elif topic == 'rainforests' or topic == 'rainforest':
-            subtopic = 'rainforests'
-        elif topic == 'deserts' or topic == 'desert':
-            subtopic = 'deserts'
-        elif topic == 'map skills' or topic == 'map':
-            subtopic = 'map skills'
-        else:
-            subtopic = 'general' # quiz with all topics
+        match topic:
+            case 'population and settlements' | 'population' | 'populations' | 'settlements' | 'settlement': # different words for same topic
+                subtopic = 'population'
+            case 'volcanoes' | 'volcano': # plural or singular
+                subtopic = 'volcanoes'
+            case 'earthquakes' | 'earthquake':
+                subtopic = 'earthquakes'
+            case 'rivers' | 'river':
+                subtopic = 'rivers'
+            case 'coasts' | 'coast':
+                subtopic = 'coasts'
+            case 'weather and climate' | 'weather' | 'climate':
+                subtopic = 'weather'
+            case 'rainforests' | 'rainforest':
+                subtopic = 'rainforests'
+            case 'deserts' | 'desert':
+                subtopic = 'deserts'
+            case 'map skills' | 'map':
+                subtopic = 'map skills'
+            case _:
+                subtopic = 'general' # quiz with all topics
 
         chosenTopic = subtopic
 
@@ -94,46 +95,47 @@ def set_subject(subject='example', topic='none'):
             multiplechoice[i] = qdata['geography'][subtopic][i]
     elif subject == 'physics':
         # different topics in physics
-        if topic == 'describing motion' or topic == 'motion':
-            subtopic = 'describing motion'
-        elif topic == 'forces and motion' or topic == 'forces' or topic == 'force':
-            subtopic = 'forces and motion'
-        elif topic == 'turning effects' or topic == 'turning effect' or topic == 'turning':
-            subtopic = 'turning effects'
-        elif topic == 'forces and matter' or topic == 'force and matter':
-            subtopic = 'forces and matter'
-        elif topic == 'energy transfer' or topic == 'energy':
-            subtopic = 'energy transfer'
-        elif topic == 'work and power' or topic == 'work':
-            subtopic = 'work and power'
-        elif topic == 'kinetic particle model of matter' or topic == 'particle model':
-            subtopic = 'kinetic particle model of matter'
-        elif topic == 'thermal properties' or topic == 'thermal property':
-            subtopic = 'thermal properties'
-        elif topic == 'thermal energy transfer' or topic == 'thermal energy':
-            subtopic = 'thermal energy transfer'
-        elif topic == 'sound':
-            subtopic = 'sound'
-        elif topic == 'light':
-            subtopic = 'kinetic particle model of matter'
-        elif topic == 'general properties of waves' or topic == 'properties of waves' or topic == 'waves':
-            subtopic = 'general properties of waves'
-        elif topic == 'electromagnetic spectrum' or topic == 'electromagnetic waves':
-            subtopic = 'electromagnetic spectrum'
-        elif topic == 'magnetism' or topic == 'magnet':
-            subtopic = 'magnetism'
-        elif topic == 'static electricity' or topic == 'static':
-            subtopic = 'static electricity'
-        elif topic == 'electrical quantities':
-            subtopic = 'electrical quantities'
-        elif topic == 'electrical circuit' or topic == 'circuit':
-            subtopic = 'electrical circuit'
-        elif topic == 'electromotive force' or topic == 'emf':
-            subtopic = 'electromotive force'
-        elif topic == 'electromagnetic induction' or topic == 'induction':
-            subtopic = 'electromagnetic induction'
-        else:
-            subtopic = 'general' # quiz with all topics
+        match topic:
+            case 'decribing motion' | 'motion':
+                subtopic = 'describing motion'
+            case 'forces and motion' | 'forces' | 'force':
+                subtopic = 'forces and motion'
+            case 'turning effects' | 'turning effect' | 'turning':
+                subtopic = 'turning effects'
+            case 'forces and matter' | 'force and matter':
+                subtopic = 'forces and matter'
+            case 'energy transfer' | 'energy':
+                subtopic = 'energy transfer'
+            case 'work and power' | 'work':
+                subtopic = 'work and power'
+            case 'kinetic particle model of matter' | 'particle model':
+                subtopic = 'kinetic particle model of matter'
+            case 'thermal properties' | 'thermal property':
+                subtopic = 'thermal properties'
+            case 'thermal energy transfer' | 'thermal energy':
+                subtopic = 'thermal energy transfer'
+            case 'sound':
+                subtopic = 'sound'
+            case 'light':
+                subtopic = 'kinetic particle model of matter'
+            case 'general properties of waves' | 'properties of waves' | 'waves':
+                subtopic = 'general properties of waves'
+            case 'electromagnetic spectrum' | 'electromagnetic waves':
+                subtopic = 'electromagnetic spectrum'
+            case 'magnetism' | 'magnet':
+                subtopic = 'magnetism'
+            case 'static electricity' | 'static':
+                subtopic = 'static electricity'
+            case 'electrical quantities':
+                subtopic = 'electrical quantities'
+            case 'electrical circuit' | 'circuit':
+                subtopic = 'electrical circuit'
+            case 'electromotive force' | 'emf':
+                subtopic = 'electromotive force'
+            case 'electromagnetic induction' | 'induction':
+                subtopic = 'electromagnetic induction'
+            case _:
+                subtopic = 'general' # quiz with all topics
 
         chosenTopic = subtopic
         
@@ -144,46 +146,47 @@ def set_subject(subject='example', topic='none'):
             multiplechoice[i] = qdata['physics'][subtopic][i]
     elif subject == 'chemistry':
         # different topics in chemistry
-        if topic == 'states of matter' or topic == 'state of matter':
-            subtopic = 'states of matter'
-        elif topic == 'atomic structure' or topic == 'atoms' or topic == 'atom':
-            subtopic = 'atomic structure'
-        elif topic == 'chemical bonding' or topic == 'chemical bonds' or topic == 'bonding' or topic == 'bonds':
-            subtopic = 'chemical bonding'
-        elif topic == 'chemical formulae and equations' or topic == 'chemical formulae' or topic == 'chemical equations' or topic == 'chemical equation' or topic == 'chemical formula' or topic == 'formula' or topic == 'equation':
-            subtopic = 'chemical formulae and equations'
-        elif topic == 'chemical calculations' or topic == 'calculations':
-            subtopic = 'chemical calculations'
-        elif topic == 'electrochemistry':
-            subtopic = 'electrochemistry'
-        elif topic == 'chemical energetics' or topic == 'energetics':
-            subtopic = 'chemical energetics'
-        elif topic == 'rates of reaction' or topic == 'rate of reaction':
-            subtopic = 'rates of reaction'
-        elif topic == 'reversible reactions' or topic == 'reverse reactions' or topic == 'reversible' or topic == 'reverse':
-            subtopic = 'thermal properties'
-        elif topic == 'redox reactions' or topic == 'reduction' or topic == 'oxidation' or topic == 'redox':
-            subtopic = 'redox reactions'
-        elif topic == 'acids and bases' or topic == 'acids' or topic == 'bases' or topic == 'alkalis':
-            subtopic = 'acids and bases'
-        elif topic == 'preperation of salts' or topic == 'salts':
-            subtopic = 'preperation of salts'
-        elif topic == 'periodic table' or topic == 'elements':
-            subtopic = 'periodic table'
-        elif topic == 'metallic elements' or topic == 'metals':
-            subtopic = 'metallic elements'
-        elif topic == 'reactivity series' or topic == 'reactivity':
-            subtopic = 'reactivity series'
-        elif topic == 'extraction and corrosion' or topic == 'extraction':
-            subtopic = 'extraction and corrosion'
-        elif topic == 'environment':
-            subtopic = 'environment'
-        elif topic == 'organic chemistry' or topic == 'organic compounds':
-            subtopic = 'organic chemistry'
-        elif topic == 'experimental design' or topic == 'experiment design' or topic == 'paper 6':
-            subtopic = 'experimental design'
-        else:
-            subtopic = 'general' # quiz with all topics
+        match topic:
+            case 'states of matter' | 'state of matter':
+                subtopic = 'states of matter'
+            case 'atomic structure' | 'atoms' | 'atom':
+                subtopic = 'atomic structure'
+            case 'chemical bonding' | 'chemical bonds' | 'bonding' | 'bonds':
+                subtopic = 'chemical bonding'
+            case 'chemical formulae and equations' | 'chemical formulae' | 'chemical equations' | 'chemical equation' | 'chemical formula' | 'formula' | 'equation':
+                subtopic = 'chemical formulae and equations'
+            case 'chemical calculations' | 'calculations':
+                subtopic = 'chemical calculations'
+            case 'electrochemistry':
+                subtopic = 'electrochemistry'
+            case 'chemical energetics' | 'energetics':
+                subtopic = 'chemical energetics'
+            case 'rates of reaction' | 'rate of reaction':
+                subtopic = 'rates of reaction'
+            case 'reversible reactions' | 'reverse reactions' | 'reversible' | 'reverse':
+                subtopic = 'thermal properties'
+            case 'redox reactions' | 'reduction' | 'oxidation' | 'redox':
+                subtopic = 'redox reactions'
+            case 'acids and bases' | 'acids' | 'bases' | 'alkalis':
+                subtopic = 'acids and bases'
+            case 'preperation of salts' | 'salts':
+                subtopic = 'preperation of salts'
+            case 'periodic table' | 'elements':
+                subtopic = 'periodic table'
+            case 'metallic elements' | 'metals':
+                subtopic = 'metallic elements'
+            case 'reactivity series' | 'reactivity':
+                subtopic = 'reactivity series'
+            case 'extraction and corrosion' | 'extraction':
+                subtopic = 'extraction and corrosion'
+            case 'environment':
+                subtopic = 'environment'
+            case 'organic chemistry' | 'organic compounds':
+                subtopic = 'organic chemistry'
+            case 'experimental design' | 'experiment design' | 'paper 6':
+                subtopic = 'experimental design'
+            case _:
+                subtopic = 'general' # quiz with all topics
 
         chosenTopic = subtopic
         
@@ -194,28 +197,29 @@ def set_subject(subject='example', topic='none'):
             multiplechoice[i] = qdata['chemistry'][subtopic][i]
     elif subject == 'computer science':
         # different topics in computer science
-        if topic == 'data representation' or topic == 'data':
-            subtopic = 'data representation'
-        elif topic == 'data transmission' or topic == 'error checking' or topic == 'transmission':
-            subtopic = 'data transmission'
-        elif topic == 'hardware':
-            subtopic = 'hardware'
-        elif topic == 'software':
-            subtopic = 'software'
-        elif topic == 'internet':
-            subtopic = 'internet'
-        elif topic == 'automation' or topic == 'automated technologies' or topic == 'ai':
-            subtopic = 'automation'
-        elif topic == 'algorithm design' or topic == 'algorithm':
-            subtopic = 'algorithm design'
-        elif topic == 'programming' or topic == 'coding':
-            subtopic = 'programming'
-        elif topic == 'databases' or topic == 'database' or topic == 'sql':
-            subtopic = 'databases'
-        elif topic == 'boolean logic' or topic == 'logic':
-            subtopic = 'boolean logic'
-        else:
-            subtopic = 'general' # quiz with all topics
+        match topic:
+            case 'data representation' | 'data':
+                subtopic = 'data representation'
+            case 'data transmission' | 'error checking' | 'transmission':
+                subtopic = 'data transmission'
+            case 'hardware':
+                subtopic = 'hardware'
+            case 'software':
+                subtopic = 'software'
+            case 'internet':
+                subtopic = 'internet'
+            case 'automation' | 'automated technologies' | 'ai':
+                subtopic = 'automation'
+            case 'algorithm design' | 'algorithm':
+                subtopic = 'algorithm design'
+            case 'programming' | 'coding':
+                subtopic = 'programming'
+            case 'databases' | 'database' | 'sql':
+                subtopic = 'databases'
+            case 'boolean logic' | 'logic':
+                subtopic = 'boolean logic'
+            case _:
+                subtopic = 'general' # quiz with all topics
 
         chosenTopic = subtopic
         
@@ -226,16 +230,17 @@ def set_subject(subject='example', topic='none'):
             multiplechoice[i] = qdata['computer science'][subtopic][i]
     elif subject == 'history':
         # different topics in computer science
-        if topic == 'early weimar germany' or topic == 'early weimar' or topic == '1918-23':
-            subtopic = 'Early Weimar Germany (1918-23)'
-        elif topic == 'golden age weimar germany' or topic == 'golden age weimar' or topic == '1924-29':
-            subtopic = 'Golden Age Weimar Germany (1924-29)'
-        elif topic == 'rise of the nazis' or topic == 'early nazis':
-            subtopic = 'Rise of the Nazis'
-        elif topic == 'nazi germany' or topic == 'nazi rule' or topic == '1933-45':
-            subtopic = 'Nazi Germany (1933-45)'
-        else:
-            subtopic = 'general' # quiz with all topics
+        match topic:
+            case 'early weimar germany' | 'early weimar' | '1918-23':
+                subtopic = 'Early Weimar Germany (1918-23)'
+            case 'golden age weimar germany' | 'golden age weimar' | '1924-29':
+                subtopic = 'Golden Age Weimar Germany (1924-29)'
+            case 'rise of the nazis' | 'early nazis':
+                subtopic = 'Rise of the Nazis'
+            case 'nazi germany' | 'nazi rule' | '1933-45':
+                subtopic = 'Nazi Germany (1933-45)'
+            case _:
+                subtopic = 'general' # quiz with all topics
 
         chosenTopic = subtopic
         
